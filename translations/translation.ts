a05d5692736783c668410ef3d63e02e9be25d540
Maykel Lopez <maykel.meneses89@gmail.com>
2023-04-02 16:29:30 +0200

    Removed translations folder

--- translations/translation.ts
+++
@@ -1,7 +0,0 @@
-import { EnglishTranslation } from "./english.translation"; 
-import { SpanishTranslation } from "./spanish.translation"; 
-
-export default { 
-  "en": EnglishTranslation,
-  "es": SpanishTranslation,
-} as const;
